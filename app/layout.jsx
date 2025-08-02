47c532854e824fa3248ca5dd33d4b48e7c65e55f
Dilsanka Sajith <Dilsanka39@gmail.com>
2025-08-03 00:07:56 +0530

    Next.js Fundamentals & Project Start

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
 import { Poppins } from 'next/font/google';
 import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const poppins = Poppins({
   variable: '--font-sans',
   subsets: ['latin'],
-  weight: ['400', '600'],
+  weight: ['400', '700'],
 });
 
 export const metadata = {
@@ -17,7 +19,9 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body className={`${poppins.className}`}>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
